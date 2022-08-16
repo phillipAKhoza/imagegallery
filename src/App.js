3660490d2e3e01e36a37d9d83ff49ca35df9aaaf
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-16 16:09:14 +0200

    adding uploadform to main app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './components/Title';
+import UploadForm from './components/uploadForm'
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm/>
     </div>
   );
 }
