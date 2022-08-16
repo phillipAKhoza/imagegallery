491ab983faa036db4851599f55090d3e9f9e8e27
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-16 14:59:33 +0200

    add firestore and storage

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getStorage} from 'firebase/storage';
+import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +17,13 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Init FireStore 
+const projectFireStore =getFirestore();
+
+// Init Storage
+
+const projectStorage = getStorage();
+
+export {projectFireStore, projectStorage};
+
