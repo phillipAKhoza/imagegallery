d25f0d610b4cc3957173e6a1b2643aaa4308ee29
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-16 16:09:30 +0200

    useState on upload form

--- src/components/uploadForm.js
+++ src/components/uploadForm.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadForm = () => {
+    const [image,setImage] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) =>{
+        // console.log("upload happend");
+        let image = e.target.files[0];
+        if (image && types.includes(image.type)) {
+            // console.log(image);
+            setImage(image);
+            setError(null)
+        }else{
+            setImage(null);
+            setError('Please select an image file (png or jpeg)');
+
+        }
+    }
     return ( 
         <form>
-            <input type="file" />
+            <input type="file" onChange={changeHandler} />
+            <div className="output">
+                {
+                    error && <div className="error">{error}</div>
+                }
+                {
+                    image && <div>{image.name}</div>
+                }
+            </div>
         </form>
      );
 }
