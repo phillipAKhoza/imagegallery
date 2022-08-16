8ea4493da7fa934feeeb03bcfe32f1b2200426b7
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-16 16:13:11 +0200

    adding a progress bar

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import useStorage from "../hooks/useStorage";
+
+const ProgressBar = () => {
+    return ( 
+        <div className="progress-bar">Progress</div>
+     );
+}
+ 
+export default ProgressBar;
