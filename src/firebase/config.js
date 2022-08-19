dabc90b4c1485b644e727023617a7b50c8c68b55
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-19 13:27:33 +0200

    firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from 'firebase/storage';
-import {getFirestore} from 'firebase/firestore';
+import {getFirestore, collection} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,11 +19,15 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Init FireStore 
-const projectFireStore =getFirestore();
+const projectFireStore = getFirestore(app);
 
 // Init Storage
 
 const projectStorage = getStorage();
 
-export {projectFireStore, projectStorage};
+// get Collection
+const imageCollection = collection(projectFireStore, 'images');
+
+
+export {projectFireStore, projectStorage, imageCollection };
 
