5e0332b5cf5f4f2c448fff67786886d4e948ea61
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-25 01:43:15 +0200

    app name change

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>EyeAMG</h1>
+      <h2>GALLERY</h2>
+      <p>Welcome to image gallery, this project is built using ReactJs and Firebase.</p>
+      <p>Upload an Image</p>
     </div>
   )
 }
