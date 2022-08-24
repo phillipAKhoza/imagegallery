25829c6aebd45c2a9479c69dc2a08da8703f398d
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-25 01:35:34 +0200

    add animation

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,7 +1,20 @@
-const ImageGrid = () => {
+import React from "react";
+import useFirestore from '../hooks/useFireStore';
+import {motion} from 'framer-motion';
+
+const ImageGrid = ({setselectedImg}) => {
+    const {docs} = useFirestore('images');
+    // console.log(docs);
     return ( 
         <div className="img-grid">
-            images
+            {docs && docs.map(doc =>(
+                <motion.div className="img-wrap" key={doc.id} onClick={()=> setselectedImg(doc.url)}
+                layout 
+                whileHover={{opacity:1}}
+                >
+                    <motion.img src={doc.url} alt="gallary" initial={{opacity:0}} animate={{opacity:1}} transition={{delay:1}}/>
+                </motion.div>
+            ))}
         </div>
      );
 }
