c24318f8104551fc6272a27eb61f2e118ac43f9b
=Gideon Ogongo <=ogjr80@gmail.com>
2022-08-25 01:35:25 +0200

    import modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title';
-import UploadForm from './components/uploadForm'
+import UploadForm from './components/uploadForm';
+import ImageGrid from './components/ImageGrid';
+import Modal from './components/modal';
 
 function App() {
+  const [selectedImg, setselectedImg] = useState(null);
   return (
     <div className="App">
       <Title/>
       <UploadForm/>
+      <ImageGrid setselectedImg={setselectedImg}/>
+      {selectedImg && <Modal selectedImg={selectedImg} setselectedImg={setselectedImg}/>}
     </div>
   );
 }
